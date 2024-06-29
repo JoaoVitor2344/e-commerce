898ac05d37809e7fca66e2d47babb2d74d8c4129
João Brito <joaovitorb2345@gmail.com>
2024-06-29 17:39:49 -0300

    feat:admin iniciado

--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,11 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [\App\Http\Controllers\DashboardController::class, 'index'])->name('dashboard.index');
+
+Route::prefix('/painel')->group(function () {
+    Route::get('/', [\App\Http\Controllers\PainelController::class, 'index'])->name('painel.index');
+
+    Route::prefix('/login')->group(function () {
+        Route::get('/', [\App\Http\Controllers\PainelController::class, 'login'])->name('painel.login');
+    });
+});
